b0e9f987a4335a806bd8c7dc0dec3b896573afe1
Lalit Chouhan <lalit.chouhan@moodys.com>
2025-09-07 14:54:56 +0530

    add new method utils

--- main_api/Utils.java
+++ main_api/Utils.java
@@ -2,4 +2,7 @@ class Utils {
     public static String getGreeting(String name) {
         return "Hello, " + name + "!";
     }
+    public static String getGreeting2(String name) {
+        return "Hello, " + name + "!";
+    }
 }
